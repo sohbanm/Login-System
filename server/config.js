a3cdaec01956e25c0241726732319f152ada5ab4
Sohban Elahi Malik <99363017+sohbanm@users.noreply.github.com>
2023-01-15 03:05:02 -0500

    server done

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET: "TXNQzh7dckQ/Kb7WXhw2IaYI/cHd7y7VRezaLHeW0Qs=",
+    EMAIL: "mathias12@ethereal.email", //testing email & password
+    PASSWORD: "D3Xe1jf96NbVjZ8dtm",
+    ATLAS_URI: "mongodb+srv://sohbanm:<admin123>@cluster0.63ioprl.mongodb.net/?retryWrites=true&w=majority"
+}
